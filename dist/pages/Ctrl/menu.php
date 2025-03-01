ca092ac2e9d8f0f8bb1de48d4da1d8c422f34aa2
enmaramos <mcdavo1309@gmail.com>
2025-03-01 08:21:04 -0600

    nuevos cambios

--- dist/pages/Ctrl/menu.php
+++ dist/pages/Ctrl/menu.php
@@ -1,42 +1,50 @@
+<!-----------------------------------------------------IINICIO DEL MENU IZQUIERDO------------------------------------------------------------>
 
-        <!-----------------------------------------------------IINICIO DEL MENU IZQUIERDO------------------------------------------------------------>
+<aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark"> <!--begin::Sidebar Brand-->
+    <div class="sidebar-brand"> <!--begin::Brand Link--> <a href="./index.html" class="brand-link"> <!--begin::Brand Image--> <img src="../../dist/assets/img/logoredondo.ico" alt="AdminLTE Logo" class="brand-image opacity-75 shadow"> <!--end::Brand Image--> <!--begin::Brand Text--><span class="brand-text fw-light"> Farmacia <br> San Francisco Javier</span> <!--end::Brand Text--> </a> <!--end::Brand Link--> </div> <!--end::Sidebar Brand--> <!--begin::Sidebar Wrapper-->
+    <div class="sidebar-wrapper">
+        <nav class="mt-2"> <!--begin::Sidebar --->
+            <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="menu" data-accordion="false">
+                <!-----------------------------------------------------------INICIO DEL MENU------------------------------------------------------------------------------>
 
-        <aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark"> <!--begin::Sidebar Brand-->
-            <div class="sidebar-brand"> <!--begin::Brand Link--> <a href="./index.html" class="brand-link"> <!--begin::Brand Image--> <img src="../../dist/assets/img/logoredondo.ico" alt="AdminLTE Logo" class="brand-image opacity-75 shadow"> <!--end::Brand Image--> <!--begin::Brand Text--><span class="brand-text fw-light" > Farmacia <br> San Francisco Javier</span> <!--end::Brand Text--> </a> <!--end::Brand Link--> </div> <!--end::Sidebar Brand--> <!--begin::Sidebar Wrapper-->
-            <div class="sidebar-wrapper">
-                  <nav class="mt-2"> <!--begin::Sidebar --->
-                    <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview" role="menu" data-accordion="false">
-                        <!-----------------------------------------------------------INICIO DEL MENU------------------------------------------------------------------------------>
-                     
-                        <!--------------------------------------------------------LO QUE EST EN USO ---------------------------------------------------->
-                       
+                <!--------------------------------------------------------LO QUE EST EN USO ---------------------------------------------------->
 
-                            <li class="nav-item">
-                                <a href="./inicio.php" class="nav-link">
-                                <i class="nav-icon far fa-image"></i>
-                                <p>
-                                    Inicio
-                                </p>
-                                </a>
-                            </li>
 
-                            <li class="nav-item menu-open">
-                                <a href="#" class="nav-link active">
-                                <i class="nav-icon bi bi-pencil-square"></i>
-                                <p>
-                                    Administracion
-                                    <i class="nav-arrow bi bi-chevron-right"></i>
-                                </p>
-                                </a>
-                                <ul class="nav nav-treeview">
-                                <li class="nav-item">
-                                    <a href="./usuarios.php" class="nav-link active">
-                                    <i class="nav-icon bi bi-circle"></i>
-                                    <p>Usuario</p>
-                                    </a>
-                                </li>
-                                </ul>
-                            </li>
-                        
-                          
-                           
+                <li class="nav-item">
+                    <a href="./inicio.php" class="nav-link">
+                        <i class="nav-icon fas fa-home"></i>
+                        <p>
+                            Inicio
+                        </p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-user-cog"></i> <!-- Icono de administración -->
+                        <p>
+                            Administración
+                            <i class="nav-arrow bi bi-chevron-right"></i> <!-- Flecha de submenú -->
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="./usuarios.php" class="nav-link">
+                                <i class="nav-icon bi bi-circle"></i>
+                                <p>Usuarios</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="./widgets/info-box.html" class="nav-link">
+                                <i class="nav-icon bi bi-circle"></i>
+                                <p>Info Box</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="./widgets/cards.html" class="nav-link">
+                                <i class="nav-icon bi bi-circle"></i>
+                                <p>Cards</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
